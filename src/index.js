537a11f65bfb6e303292735774df20277e6989ae
Scott Senkier <scott.senkier@summit.local>
2018-12-07 11:21:11 -0500

    changed to functional component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 
-export default class extends Component {
-  render() {
-    return (
-      <div>
-        <h2>{this.props.message}</h2>
-      </div>
-    );
-  }
+export default function({ message = "Michigan Wolverines" }) {
+  return (
+    <div>
+      <h2>{message}</h2>
+    </div>
+  );
 }
